2464a73c9e599440f79b9c1f717ec61bb7aed9c3
saratansky <saratansky@gmail.com>
2022-01-29 16:40:22 +0300

    added consts in internal settings

---
+++ core/base/internal_settings.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('VG_ACCESS') or die ("Access denied");
+
+const TEMPLATE = 'templates/default';
+const ADMIN_TEMPLATES = 'core/admin/views/';
+
+const COOCKIE_VERSION = '2.0.0';
+const CREATE_KEY = '';
+const COOCKIE_TIME = '60';
