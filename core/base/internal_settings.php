552f27c080e5cc4f64730c7f2bb72a0bd4d9437c
saratansky <saratansky@gmail.com>
2022-01-29 16:45:12 +0300

    added consts in internal settings

--- core/base/internal_settings.php
+++ core/base/internal_settings.php
@@ -7,4 +7,19 @@ const ADMIN_TEMPLATES = 'core/admin/views/';
 
 const COOCKIE_VERSION = '2.0.0';
 const CREATE_KEY = '';
-const COOCKIE_TIME = '60';
+const COOCKIE_TIME = 60;
+const BOCK_TIME = 3;
+
+const QTY = 8;
+const QTY_LINKS = 3;
+
+const ADMIN_CSS_JS = [
+    'styles' => [],
+    'scripts'=> []
+];
+
+const USER_CSS_JS = [
+    'styles' => [],
+    'scripts'=> []
+];
+
