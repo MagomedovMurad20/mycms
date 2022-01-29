33fe4a4f71636c10deffd2c1ff890474e414f047
saratansky <saratansky@gmail.com>
2022-01-29 16:36:37 +0300

    added consts for db in config php

--- config.php
+++ config.php
@@ -2,4 +2,12 @@
 
 defined('VG_ACCESS') or die ("Access denied");
 
-echo "hello!!!!";
+consT SITE_URL = 'http://mycms';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'mycms';
+
+
--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 
 define ('VG_ACCESS', true);
 header('Content-Type:text/html; charset-utf-8');
+session_start();
 
 require_once 'config.php';
+require_once 'core/base/internal_settings.php';
